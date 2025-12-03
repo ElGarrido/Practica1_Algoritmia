6b6b601a555d24f4664db6ac59d9a8164aa2799f
ElGarrido <120379942+ElGarrido@users.noreply.github.com>
2025-12-03 14:04:33 +0100

    Implement Estudiante class with course enrollment

--- Practica1Algoritmia/src/practica1algoritmia/Estudiante.java
+++ Practica1Algoritmia/src/practica1algoritmia/Estudiante.java
@@ -1,7 +1,44 @@
 
 package practica1algoritmia;
 
-public class Estudiante {
-    // Con nodos
+public class Estudiante implements Interface_Elemento {
+    
+    private String nombre;
+    private String DNI;
+    
+    private Interface_Lista cursosMatriculados;
+    
+    public Estudiante (String nombre, String DNI){
+        this.nombre = nombre;
+        this.DNI = DNI;
+        
+        this.cursosMatriculados = new ListaAsignatura();
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDni() {
+        return DNI;
+    }
+    
+    public void Matricular(Asignatura asignatura){
+        
+        ReferenciaAsignatura ref = new ReferenciaAsignatura(asignatura);
+        this.cursosMatriculados.insertar(ref);
+        
+    }
+
+    @Override
+    public String getIdentificador() {
+        return DNI;
+    }
+    
+    @Override
+    public String toString() {
+        return "Estudiante: " + nombre + " | DNI: " + DNI;
+    }
+    
     
 }
